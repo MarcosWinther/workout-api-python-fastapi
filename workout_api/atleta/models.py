cad922fdf8b8ed519a1bd9a71c9649fd233f59e0
MarcosWinther <winthermarcos@gmail.com>
2026-01-06 14:07:51 -0300

    ✨ feat(atleta): adicionar relacionamento com categoria
    Adiciona relacionamento entre AtletaModel e CategoriaModel:
    
    - workout_api/atleta/models.py: adicionados categoria_id (FK) e categoria (relationship), import de CategoriaModel, ajustes de imports.
    - workout_api/categorias/: adicionados models.py, schemas.py e __init__.py com definição de CategoriaModel e schema.

--- workout_api/atleta/models.py
+++ workout_api/atleta/models.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 
-from sqlalchemy import DateTime, Float, Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import DateTime, Float, ForeignKey, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from workout_api.categorias.models import CategoriaModel
 from workout_api.contrib.models import BaseModel
 
 
@@ -16,3 +17,5 @@ class AtletaModel(BaseModel):
     altura: Mapped[float] = mapped_column(Float, nullable=False)
     sexo: Mapped[str] = mapped_column(String(1), nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    categoria: Mapped[CategoriaModel] = relationship(back_populates='atleta')
+    categoria_id: Mapped[int] = mapped_column(ForeignKey('categorias.pk_id'))
